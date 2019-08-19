7282706d1216b040f8b77ecf9e101859da3e708f
Bjoern Strausmann <bjoern@strausmann.net>
2019-08-19 12:45:32 +0200

    Update composer.json

--- public/index.php
+++ public/index.php
@@ -7,12 +7,28 @@ use UniFi_API\Client;
 // load the class using the composer autoloader
 require_once('../vendor/autoload.php');
 
-$whoops = new \Whoops\Run;
-$whoops->prependHandler(new \Whoops\Handler\PrettyPageHandler);
-$whoops->register();
+if (class_exists("\Whoops\Run")){
+    $whoops = new \Whoops\Run;
+    $whoops->prependHandler(new \Whoops\Handler\PrettyPageHandler);
+    $whoops->register();
+}
 
 $dotenv = Dotenv::create(dirname(dirname(__FILE__)));
 $dotenv->load(true);
+
+if (file_exists($_SERVER['DOCUMENT_ROOT'].'/composer.json')) {
+  // ...
+  ech;
+}
+
+var_dump(class_exists("\Whoops\Run"));
+
+echo $google;
+
+echo $projectRootPath = realpath(\Composer\Factory::getComposerFile());
+
+print_r(pathinfo(dirname(\Composer\Factory::getComposerFile())));
+print_r($_SERVER);
 
 date_default_timezone_set(getenv('TIMEZONE'));
 header('Content-Type: application/json');
@@ -26,7 +42,7 @@ $meshGenerator = new MeshviewerGenerator();
 
 #print_r($meshGenerator->getAccessPointMetaDataBySerial('18E829E6D74F'));
 
-print_r($meshGenerator->outputMeshviewerList());
+#print_r($meshGenerator->outputMeshviewerList());
 
 #print_r($meshGenerator->outputNodelist());
 
