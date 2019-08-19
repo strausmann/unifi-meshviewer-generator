a3f3eab8cf1a811cc5ecc89727d48ec52668f7d5
Bjoern Strausmann <bjoern@strausmann.net>
2019-08-19 12:47:18 +0200

    Cleanup public/index.php

--- public/index.php
+++ public/index.php
@@ -2,54 +2,34 @@
 
 use ISPServerfarm\UnifiMeshviewer\MeshviewerGenerator;
 use Dotenv\Dotenv;
-use UniFi_API\Client;
 
 // load the class using the composer autoloader
 require_once('../vendor/autoload.php');
 
+// load Whoops
 if (class_exists("\Whoops\Run")){
     $whoops = new \Whoops\Run;
     $whoops->prependHandler(new \Whoops\Handler\PrettyPageHandler);
     $whoops->register();
 }
 
+// load dotenv and the .env file.
 $dotenv = Dotenv::create(dirname(dirname(__FILE__)));
 $dotenv->load(true);
 
-if (file_exists($_SERVER['DOCUMENT_ROOT'].'/composer.json')) {
-  // ...
-  ech;
-}
-
-var_dump(class_exists("\Whoops\Run"));
-
-echo $google;
-
-echo $projectRootPath = realpath(\Composer\Factory::getComposerFile());
-
-print_r(pathinfo(dirname(\Composer\Factory::getComposerFile())));
-print_r($_SERVER);
-
+// set TimeZone
 date_default_timezone_set(getenv('TIMEZONE'));
+
+// set Content Type to application/json
 header('Content-Type: application/json');
 
-
+// Initiate the MeshviewerGenerator
 $meshGenerator = new MeshviewerGenerator();
-#$meshGenerator->enableDebug();
-#echo "<pre>";
-#print_r($meshGenerator->buildNodes());
-#exit();
 
-#print_r($meshGenerator->getAccessPointMetaDataBySerial('18E829E6D74F'));
-
-#print_r($meshGenerator->outputMeshviewerList());
-
-#print_r($meshGenerator->outputNodelist());
-
+// generate the NodeList JSON file in the data/ folder
 $meshGenerator->writeNodeListFile();
 
+// generate the Meshviewer JSON file in the data/ folder
 $meshGenerator->writeMeshviewerListFile();
-
-#print_r($meshGenerator->getLinks());
 
 ?>
