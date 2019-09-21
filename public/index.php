70c58b92187751ad79193a6fd67d838d1662e9f7
Björn Strausmann <bjoern@strausmann.net>
2019-09-21 12:48:01 +0200

    Resolve "Aktuelle Version in die Cronjob Antwort integrieren"

--- public/index.php
+++ public/index.php
@@ -23,10 +23,6 @@ date_default_timezone_set(getenv('TIMEZONE'));
 // Initiate the MeshviewerGenerator
 $meshGenerator = new MeshviewerGenerator();
 
-// Test
-#echo "<pre>";
-#$meshGenerator->getGateway("18e8295ccf02");
-
 // set Content Type to application/json
 header('Content-Type: application/json');
 
